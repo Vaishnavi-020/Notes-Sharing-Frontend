d20f60524f81288fbdf63ac49323857b27d9d6b8
Vaishnavi <sinhavaishnavi020@gmail.com>
2026-03-05 14:23:36 +0530

    feat:Added search bar in pvt notes

--- src/pages/MyNotes.jsx
+++ src/pages/MyNotes.jsx
@@ -8,12 +8,24 @@ const MyNotes=()=>{
     const[loading,setLoading]=useState(false)
     const [notes,setNotes]=useState([])
     const [page,setPage]=useState(1)
-    // const[query,setQuery]=useState("")
+    const[query,setQuery]=useState("")
 
     const {user,loading:authLoading}=useAuth()
     const navigate=useNavigate()
 
     const LIMIT=10
+
+    // const fetchPvtNotes=async()=>{
+    //     try{
+    //         setLoading(true)
+    //         const res=await api.get("/notes/my_notes",{params:{page,limit:LIMIT}})
+    //         setNotes(res.data.items)
+    //     }catch(err){
+    //         console.error("Error fetching pvt notes",err)
+    //     }finally{
+    //         setLoading(false)
+    //     }
+    // }
 
     const onDelete=async(id)=>{
             try{
@@ -36,7 +48,20 @@ const MyNotes=()=>{
             const res=await api.get("/notes/my_notes",{params:{page,limit:LIMIT}})
             setNotes(res.data.items)
         }catch(err){
-            console.error("Error fetching public notes",err)
+            console.error("Error fetching private notes",err)
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    const searchNotes=async()=>{
+        try{
+            setLoading(true)
+            setPage(1)
+            const res=await api.get("/notes/search",{params:{q:query,page:1,limit:LIMIT}})
+            setNotes(res.data.items)
+        }catch(err){
+            console.error("Search failed",err)
         }finally{
             setLoading(false)
         }
@@ -52,6 +77,20 @@ const MyNotes=()=>{
         <div className="min-h-screen flex flex-col p-6 max-w-6xl mx-auto">
             <div className="flex-grow">
                 <h1 className="font-bold text-2xl text-stone-800 mb-8 p-2 border-b border-slate-500">My Notes</h1>
+
+            <div className="flex gap-2 mb-6">
+                <input
+                type="text"
+                placeholder="Search Notes..."
+                value={query}
+                onChange={(e)=>setQuery(e.target.value)}
+                className="border p-2 rounded w-full" />
+                <button onClick={searchNotes}
+                className="px-4 py-2 bg-green-600 text-white rounded">
+                    Search
+                </button>
+            </div>
+
 
                 {loading && <p>Loading Notes...</p>}
             {!loading && notes.length===0 && <p>No notes found!</p>}
